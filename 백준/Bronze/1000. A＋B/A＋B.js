098853072cb93d0d64f487336c5a3a6c9660b3ee
won a-jin <ajinwon1@gmail.com>
2025-06-24 21:09:41 +0900

    [Bronze V] Title: A+B, Time: 96 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim();
+
+// 한 줄 입력 처리 (공백으로 구분된 값)
+const numbers = input.split(' ').map(Number);
+
+// 문제 풀이 함수
+function solution(numbers) {
+  // 여기에 코드 작성
+  return numbers[0] + numbers[1]; 
+}
+
+// 결과 출력
+console.log(solution(numbers)); 
