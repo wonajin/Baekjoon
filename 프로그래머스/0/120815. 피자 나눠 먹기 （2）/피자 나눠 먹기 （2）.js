f50b47bed4716c5b720f17cdec8b86549d63e392
won a-jin <ajinwon1@gmail.com>
2025-06-25 22:56:00 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -0,0 +1,21 @@
+function solution(n) {
+    var answer = 0;
+    
+    //6과 n의 최소 공배수를 구한 후 그걸 6으로 다시 나눠줘야한다
+    
+    const gcd = (a, b) => {
+            if (b === 0) return a;
+            return gcd(b, a % b);
+    };
+    // 최소공배수 구하기
+    const lcm= (a,b )=>{
+        return (a*b)/ gcd(a,b);
+    };
+    
+    
+
+    answer=lcm(6,n)/6;
+    return answer;
+}
+
+
