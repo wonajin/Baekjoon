ed89b04c05be27fabcd50922be90bc50dc162ca6
won a-jin <ajinwon1@gmail.com>
2025-07-01 01:59:30 +0900

    [level 0] Title: 모스부호 (1), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,15 @@
+function solution(letter) {
+  const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+  };
+  
+  const split = letter.split(' ');
+
+  const decoded = split.map(code => morse[code]);
+
+  return decoded.join('');
+}
