d21b9cc614c94b86ac7e05bd8561a7e3a8c32792
won a-jin <ajinwon1@gmail.com>
2025-07-09 00:31:59 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,20 @@
+function solution(numbers) {
+  const map = {
+    zero: '0',
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
+  };
+
+  for (let key in map) {
+    numbers = numbers.split(key).join(map[key]);
+  }
+
+  return Number(numbers);
+}
