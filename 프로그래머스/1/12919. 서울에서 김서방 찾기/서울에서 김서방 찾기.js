240861f9ffd987ec07ea4032786d28ba357e0ea5
won a-jin <ajinwon1@gmail.com>
2025-08-15 02:39:18 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,10 @@
+function solution(seoul) {
+    //var answer = '';
+    let idx;
+    for(let i=0; i <seoul.length;i++){
+        if(seoul[i]==="Kim"){
+            return(`김서방은 ${i}에 있다`)
+        }
+    }
+   // return answer;
+}
