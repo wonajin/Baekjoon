15a17773a6a6a810ac9f2ab1cbf9f71d697e27b5
won a-jin <ajinwon1@gmail.com>
2025-08-19 10:52:06 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.56 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,15 @@
+function solution(numbers) {
+    let result = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            result.push(numbers[i] + numbers[j]);
+        }
+    }
+
+    result = [...new Set(result)];
+
+    result.sort((a, b) => a - b);
+
+    return result;
+}
