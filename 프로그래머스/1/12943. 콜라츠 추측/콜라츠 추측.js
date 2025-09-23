d8621635c1e11c24ce669370b90125940385dc09
won a-jin <ajinwon1@gmail.com>
2025-09-23 10:50:17 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,17 @@
+function solution(num) {
+    let count = 0;
+
+    while (num !== 1) {
+        if (count >= 500) return -1;
+
+        if (num % 2 === 0) {
+            num = num / 2; 
+        } else {
+            num = num * 3 + 1; 
+        }
+
+        count++;
+    }
+
+    return count;
+}
